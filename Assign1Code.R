44760177b7bfb51597e2c5ad7bee3669453597a1
congux <congli@miami.edu>
2023-01-08 09:44:19 -0500

    commit more changes

--- Assign1Code.R
+++ Assign1Code.R
@@ -1,5 +1,6 @@
 ## Assignment2 Code
 
+## This makeCacheMatrix function is to create a matrix that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -14,6 +15,9 @@ makeCacheMatrix <- function(x = matrix()) {
        setInverse = setInverse,
        getInverse = getInverse)
 }
+
+## This cacheSolve function is to compute the inverse of the maxtrix created by the makeCacheMatrix fucntion.
+## If the inverse has already been calculated,the cacheSolve will retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
   i <- x$getInverse()
   if (!is.null(i)) {
@@ -21,7 +25,7 @@ cacheSolve <- function(x, ...) {
     return(i)
   }
   mdata <- x$get()
-  inv <- solve(mdata, ...)
+  i <- solve(mdata, ...)
   x$setInverse(i)
   i
 }
